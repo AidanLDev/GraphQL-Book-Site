753c40f0a8b4fc413a0231d7c155edd85344ad96
Aidan <aidanlowson@gmail.com>
2019-11-10 17:40:09 +0000

    Adding mongoDB connection

--- api/index.js
+++ api/index.js
@@ -2,8 +2,25 @@ const express = require("express");
 const PORT = 4000;
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const DBPassword = require("./dbpw");
 
 const app = express();
+
+/*
+//  Save the following (with your DB users PW) in a javaScript file called dbpw
+  const pw = "<your PW>";
+  module.exports = pw;
+*/
+
+/*  Connecting to MongoDB  */
+mongoose.connect(
+  `mongodb+srv://aidanrw${DBPassword}@graphql-vowny.mongodb.net/test?retryWrites=true&w=majority`
+);
+
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
