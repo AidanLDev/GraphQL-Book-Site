aaa4b1b9a77acb84f6c1c4148412f39f208f946a
Aidan <aidansoftwaredev@gmail.com>
2019-11-10 12:35:12 +0000

    setting up dummy data and returning it with lodash

--- api/schema/schema.js
+++ api/schema/schema.js
@@ -1,6 +1,13 @@
 const graphql = require("graphql");
-
+const _ = require("lodash");
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// Dummy data
+let books = [
+  { name: "1984", genre: "Distopia", id: "1" },
+  { name: "Brave New World", genre: "Distopia", id: "2" },
+  { name: "Neuromancer", genre: "Sci-Fi", id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -11,7 +18,7 @@ const BookType = new GraphQLObjectType({
   })
 });
 
-// Query used from the frontend to get db data
+// Query used to transfer the 'graph' and interact with data
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: () => ({
@@ -24,6 +31,7 @@ const RootQuery = new GraphQLObjectType({
       // Resolve gets data from the db
       resolve(parent, args) {
         // args.id
+        return _.find(books, { id: args.id });
       }
     }
   })
